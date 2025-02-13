4354992c9b6ebcbe81184e85aed1d72fc531a7fe
Ravi <108709054+rkvmar@users.noreply.github.com>
2025-02-12 16:56:45 -0800

    Update boot.py
    12 buttons??

--- boot.py
+++ boot.py
@@ -6,6 +6,6 @@ usb_hid.enable(
         usb_hid.Device.KEYBOARD,
         usb_hid.Device.MOUSE,
         usb_hid.Device.CONSUMER_CONTROL,
-        create_joystick(axes=0, buttons=3, hats=0),
+        create_joystick(axes=0, buttons=12, hats=0),
     )
 )
